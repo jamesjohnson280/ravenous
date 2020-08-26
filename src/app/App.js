fc06dfe032681d8ab89461b3d182f23cfeb60203
James Johnson <james.johnson280@gmail.com>
2020-08-26 11:26:19 -0400

    Format app markup

--- src/app/App.js
+++ src/app/App.js
@@ -27,10 +27,10 @@ const businesses = [
 function App() {
   return (
     <div class="App">
-    <h1>ravenous</h1>
-    <SearchBar />
-    <BusinessList businesses={businesses} />
-  </div>
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList businesses={businesses} />
+    </div>
   );
 }
 
